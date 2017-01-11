745e2cc2b50c46bda8c4fdaeef0ece688ccee3a8
Eka Putra <anonym415@gmail.com>
2017-01-11 21:25:06 +0700

    move func main to bottom

--- 01_struct/main.go
+++ 01_struct/main.go
@@ -11,6 +11,24 @@ type Pokemon struct {
 	CountTrng int
 }
 
+func (p Pokemon) Ability() {
+	fmt.Printf("HP\t: %f\n", p.HP)
+	fmt.Printf("Defense\t: %f\n", p.Defense)
+	fmt.Printf("Speed\t: %f\n", p.Speed)
+}
+
+func (p *Pokemon) Training() {
+	p.HP += p.HP * 0.05
+	p.Attack += p.Attack * 0.05
+	p.Defense += p.Defense * 0.05
+	p.Speed += p.Speed * 0.05
+	p.CountTrng += 1
+}
+
+func (p Pokemon) StoneAttack() float64 {
+	return p.Attack + 50
+}
+
 func main() {
 	pikachu := Pokemon{
 		Name:    "Pikachu",
@@ -37,22 +55,4 @@ func main() {
 			fmt.Printf("Attack\t: %f\n", pikachu.StoneAttack())
 		}
 	}
-}
-
-func (p Pokemon) Ability() {
-	fmt.Printf("HP\t: %f\n", p.HP)
-	fmt.Printf("Defense\t: %f\n", p.Defense)
-	fmt.Printf("Speed\t: %f\n", p.Speed)
-}
-
-func (p *Pokemon) Training() {
-	p.HP += p.HP * 0.05
-	p.Attack += p.Attack * 0.05
-	p.Defense += p.Defense * 0.05
-	p.Speed += p.Speed * 0.05
-	p.CountTrng += 1
-}
-
-func (p Pokemon) StoneAttack() float64 {
-	return p.Attack + 50
 }
